4dd301cc634792d1c57ecf5c514287dbe290d1de
MacQuy <macquydev77@gmail.com>
2025-10-24 10:51:13 +0700

    Setup LiveKit demo

--- frontend/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import StreamerDashboard from "./components/StreamerDashboard";
-
-function App() {
-  return <StreamerDashboard />;
-}
-
-export default App;
--- frontend/src/services/socket.ts
+++
@@ -1,6 +0,0 @@
-import { io } from "socket.io-client";
-
-// Nếu bạn có backend sau này thì đổi thành http://localhost:5000
-export const socket = io("http://localhost:3001", {
-  transports: ["websocket"],
-});
